d0f71cc3bbf7e9a79518183eb068ffe0e0dedbba
Mauricio Serna <mauricioserf@gmail.com>
2017-08-15 22:07:26 -0500

    [Add] component state for contacts

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -4,17 +4,33 @@ import Contact from './Contact';
 import data from './data';
 
 class Layout extends Component {
+  componentWillMount () {
+    this.setState({
+      contacts: data,
+    })
+  }
+
   addContact = (event) => {
     event.preventDefault();
-    console.log('Click');
+
+    const contacts = this.state.contacts;
+    const newId = contacts[contacts.length - 1].id + 1;
+
+    this.setState({
+      contacts: contacts.concat({ 
+        id: newId,
+        name: `New contact ${newId}`, 
+        email: `${newId}@exampe.com`
+      })
+    });
   }
 
   render() {
     return (
       <div id='Layout'>
-        <a href='#' className='pure-button' onClick={this.addContact}> Add contact </a>
+        <a className='pure-button' onClick={this.addContact}> Add contact </a>
         <div className='pure-g'>
-          {data.map(info => <Contact key={info.id} {...info} />)}
+          {this.state.contacts.map(info => <Contact key={info.id} {...info} />)}
         </div>
       </div>
     );
