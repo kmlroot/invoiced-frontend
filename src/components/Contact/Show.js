412eb35267f5f479c633fbf9134ae858dd87d989
Mauricio Serna <mauricioserf@gmail.com>
2017-08-17 15:48:32 -0500

    [Add] show contact page

--- src/components/Contact/Show.js
+++ src/components/Contact/Show.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
+import data from '../data';
+
 export class Show extends Component {
+
+	componentWillMount () {
+		const contactId = this.props.match.params.contactId;
+
+		this.setState({
+			contact: data.filter(contact => contact.id === parseInt(contactId, 10))[0],
+		});
+	}
+
 	render() {
 		return (
 			<div id='show'>
-				<h2> Contact detail </h2>
+				<h2> {this.state.contact.name} </h2>
 			</div>
 		);
 	}
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,22 @@
 import React from 'react';
+
 import { 
 	BrowserRouter as Router,
 	Route,
 	Switch
 } from 'react-router-dom';
 
+import createHistory from 'history/createBrowserHistory'
+
 import Collection from './components/Collection/';
 import Show from './components/Contact/Show';
 import NotFound from './components/NotFound/';
 
+const history = createHistory();
+
 const Routes = () => {
 	return (
-		<Router>
+		<Router history={history} >
 			<Switch>
 				<Route exact path='/' component={Collection} />
 				<Route path='/contacts/:contactId' component={Show} />
